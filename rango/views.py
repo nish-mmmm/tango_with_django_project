d35228fe9845771a568f182fd2d3e06f097c11eb
“Tanishaa <tanishaa2005@icloud.com>
2025-02-12 23:59:12 +0000

    Finished chapter 5

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,6 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br> <a href='/rango/'>Index</a>")
-
+    return render(request, 'rango/about.html')
 
 # Create your views here.
