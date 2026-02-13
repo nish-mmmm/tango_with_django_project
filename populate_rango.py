1a121cbebabd54b648333b324ba6d251cc2693c3
“Tanishaa <2894464g@student.gla.ac.uk>
2026-02-13 05:30:23 +0000

    Chapter 5

---
+++ populate_rango.py
@@ -0,0 +1,67 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+from django.template.defaultfilters import slugify
+import django 
+django.setup()
+from rango.models import Category, Page
+import random
+
+def populate():
+    python_pages = [
+        {'title': 'Official Python Tutorial',
+         'url':'http://docs.python.org/3/tutorial/','views': 100},
+        {'title':'How to Think like a Computer Scientist',
+         'url':'http://www.greenteapress.com/thinkpython/','views': 150},
+        {'title':'Learn Python in 10 Minutes',
+          'url':'http://www.korokithakis.net/tutorials/python/', 'views': 75} ]
+
+    django_pages = [
+        {'title':'Official Django Tutorial',
+        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': 120},
+        {'title':'Django Rocks',
+        'url':'http://www.djangorocks.com/','views': 110},
+        {'title':'How to Tango with Django',
+        'url':'http://www.tangowithdjango.com/' ,'views': 130} ]
+
+    other_pages = [
+        {'title':'Bottle',
+        'url':'http://bottlepy.org/docs/dev/','views': 90},
+        {'title':'Flask',
+        'url':'http://flask.pocoo.org','views': 95} ]
+
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+        'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
+    }
+
+
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
+        for p in cat_data['pages']:
+            add_page(c, p['title'], p['url'], p['views'])
+
+ # Print out the categories we have added.
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print(f'{c}: {p}')
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_cat(name,views=0, likes=0):
+    slug = slugify(name)
+    c, created = Category.objects.get_or_create(name=name, slug=slug)
+   # c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
+if __name__ == '__main__':
+    print('Starting Rango population script...')
+    populate()
